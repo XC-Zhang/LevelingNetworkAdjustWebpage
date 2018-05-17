273edd4ea6073e077250d40ca1667b0b8564de43
xc-zhang-surface <studentzxc@live.cn>
2018-05-18 02:18:14 +0800

    Feature: Select known benchmarks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { CsvComponent } from './csv.component';
@@ -16,7 +21,12 @@ import { BmsTableComponent } from './bms.table.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatTableModule
   ],
   providers: [],
